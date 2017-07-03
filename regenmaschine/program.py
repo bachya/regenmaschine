8a4c2b119845469ef934de6f5ffee06cf604be3b
Aaron Bach <bachya1208@gmail.com>
2017-07-03 12:13:08 -0600

    Figuring out proper way to override BaseAPI methods

--- regenmaschine/program.py
+++ regenmaschine/program.py
@@ -15,43 +15,36 @@ class Programs(api.BaseAPI):
 
     def __init__(self, *args, **kwargs):
         """ Initialize! """
-        self.caller = super(Programs, self)
-        super(Programs, self).__init__(*args, **kwargs)
+        self.parent = super(Programs, self)
+        self.parent.__init__(*args, **kwargs)
 
     @api.broken_remote_api
     def _broken_remote_api_test(self):  #pylint: disable=no-self-use
         """ Pure test method to test the broken_remote_api decorator """
         return {'status': 'ok'}
 
-    @staticmethod
-    def _build_start_stop_endpoint(program_id, action):
-        """ To make Code Climate happy... """
-        return 'program/{}/{}'.format(program_id, action)
-
     def all(self):
         """ Returns all programs """
-        return self.caller.get('program').body
+        return self.parent.get('program').body
 
     def get(self, program_id):  # pylint: disable=arguments-differ
         """ Returns information for a specific program """
-        return self.caller.get('program/{}'.format(program_id)).body
+        return self.parent.get('program/{}'.format(program_id)).body
 
     def next(self):
         """ Returns the next run date/time for all programs """
-        return self.caller.get('program/nextrun').body
+        return self.parent.get('program/nextrun').body
 
     def running(self):
         """ Returns all running programs """
-        return self.caller.get('watering/program').body
+        return self.parent.get('watering/program').body
 
     @api.broken_remote_api
     def start(self, program_id):
         """ Starts a program """
-        return self.caller.post(
-            self._build_start_stop_endpoint(program_id, 'start')).body
+        return self.parent.post('program/{}/start'.format(program_id)).body
 
     @api.broken_remote_api
     def stop(self, program_id):
         """ Stops a program """
-        return self.caller.post(
-            self._build_start_stop_endpoint(program_id, 'stop')).body
+        return self.parent.post('program/{}/stop'.format(program_id)).body
--- regenmaschine/zone.py
+++ regenmaschine/zone.py
@@ -13,32 +13,36 @@ import regenmaschine.api as api
 class Zones(api.BaseAPI):
     """ An object to list, interact with, etc. zones """
 
+    def __init__(self, *args, **kwargs):
+        """ Initialize! """
+        self.parent = super(Zones, self)
+        self.parent.__init__(*args, **kwargs)
+
     def all(self, properties=False):
         """ Returns all zones (optionally showing advanced properties) """
         if properties:
-            return super(Zones, self).get('zone/properties').body
+            return self.parent.get('zone/properties').body
 
-        return super(Zones, self).get('zone').body
+        return self.parent.get('zone').body
 
     def get(self, zone_id, properties=False):  # pylint: disable=arguments-differ
         """ Returns information for a specific zone """
         if properties:
-            return super(Zones,
-                         self).get('zone/{}/properties'.format(zone_id)).body
+            return self.parent.get('zone/{}/properties'.format(zone_id)).body
 
-        return super(Zones, self).get('zone/{}'.format(zone_id)).body
+        return self.parent.get('zone/{}'.format(zone_id)).body
 
     def simulate(self, zone_data):
         """
         Simulates a zone activity (based on advanced zone properties)
         """
-        return super(Zones, self).post('zone/simulate', data=zone_data).body
+        return self.parent.post('zone/simulate', data=zone_data).body
 
     def start(self, zone_id, seconds):
         """ Starts a zone for a specific number of seconds """
-        return super(Zones, self).post(
+        return self.parent.post(
             'zone/{}/start'.format(zone_id), data={'time': seconds}).body
 
     def stop(self, zone_id):
         """ Stops a zone """
-        return super(Zones, self).post('zone/{}/stop'.format(zone_id)).body
+        return self.parent.post('zone/{}/stop'.format(zone_id)).body
