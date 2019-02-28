4e37accf492d46af05d9cda441d7be5c912bbe07
Aaron Bach <bachya1208@gmail.com>
2019-02-28 15:31:09 -0700

    Split tests into bite-sized pieces

--- tests/test_watering.py
+++ tests/test_watering.py
@@ -17,11 +17,9 @@ from .fixtures.watering import *
 
 
 @pytest.mark.asyncio
-async def test_endpoints(
-        aresponses, authenticated_client, event_loop, watering_log_json,
-        watering_past_json, watering_pause_json, watering_queue_json,
-        watering_stopall_json):
-    """Test all endpoints."""
+async def test_watering_log_details(
+        aresponses, authenticated_client, event_loop, watering_log_json):
+    """Test getting watering log details."""
     today = datetime.date.today()
     today_str = today.strftime('%Y-%m-%d')
 
@@ -31,33 +29,7 @@ async def test_endpoints(
             '/api/4/watering/log/details/{0}/{1}'.format(today_str, 2), 'get',
             aresponses.Response(
                 text=json.dumps(watering_log_json), status=200))
-        authenticated_client.add(
-            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/pauseall',
-            'post',
-            aresponses.Response(
-                text=json.dumps(watering_pause_json), status=200))
-        authenticated_client.add(
-            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/pauseall',
-            'post',
-            aresponses.Response(
-                text=json.dumps(watering_pause_json), status=200))
-        authenticated_client.add(
-            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/queue',
-            'get',
-            aresponses.Response(
-                text=json.dumps(watering_queue_json), status=200))
-        authenticated_client.add(
-            '{0}:{1}'.format(TEST_HOST, TEST_PORT),
-            '/api/4/watering/past/{0}/{1}'.format(today_str, 2), 'get',
-            aresponses.Response(
-                text=json.dumps(watering_past_json), status=200))
-        authenticated_client.add(
-            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/stopall',
-            'post',
-            aresponses.Response(
-                text=json.dumps(watering_stopall_json), status=200))
 
-        # pylint: disable=protected-access
         async with aiohttp.ClientSession(loop=event_loop) as websession:
             client = await login(
                 TEST_HOST,
@@ -69,17 +41,105 @@ async def test_endpoints(
             data = await client.watering.log(today, 2, details=True)
             assert len(data) == 2
 
+
+@pytest.mark.asyncio
+async def test_watering_pause(
+        aresponses, authenticated_client, event_loop, watering_pause_json):
+    """Test pausing and unpausing watering."""
+    async with authenticated_client:
+        authenticated_client.add(
+            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/pauseall',
+            'post',
+            aresponses.Response(
+                text=json.dumps(watering_pause_json), status=200))
+        authenticated_client.add(
+            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/pauseall',
+            'post',
+            aresponses.Response(
+                text=json.dumps(watering_pause_json), status=200))
+
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            client = await login(
+                TEST_HOST,
+                TEST_PASSWORD,
+                websession,
+                port=TEST_PORT,
+                ssl=False)
+
             data = await client.watering.pause_all(30)
             assert data['message'] == 'OK'
 
             data = await client.watering.unpause_all()
             assert data['message'] == 'OK'
 
+
+@pytest.mark.asyncio
+async def test_watering_queue(
+        aresponses, authenticated_client, event_loop, watering_queue_json):
+    """Test getting the watering queue."""
+    async with authenticated_client:
+        authenticated_client.add(
+            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/queue',
+            'get',
+            aresponses.Response(
+                text=json.dumps(watering_queue_json), status=200))
+
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            client = await login(
+                TEST_HOST,
+                TEST_PASSWORD,
+                websession,
+                port=TEST_PORT,
+                ssl=False)
+
             data = await client.watering.queue()
             assert not data
 
+
+@pytest.mark.asyncio
+async def test_watering_past(
+        aresponses, authenticated_client, event_loop, watering_past_json):
+    """Test gettinng info on past watering runs."""
+    today = datetime.date.today()
+    today_str = today.strftime('%Y-%m-%d')
+
+    async with authenticated_client:
+        authenticated_client.add(
+            '{0}:{1}'.format(TEST_HOST, TEST_PORT),
+            '/api/4/watering/past/{0}/{1}'.format(today_str, 2), 'get',
+            aresponses.Response(
+                text=json.dumps(watering_past_json), status=200))
+
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            client = await login(
+                TEST_HOST,
+                TEST_PASSWORD,
+                websession,
+                port=TEST_PORT,
+                ssl=False)
+
             data = await client.watering.runs(today, 2)
             assert len(data) == 8
+
+
+@pytest.mark.asyncio
+async def test_watering_stop(
+        aresponses, authenticated_client, event_loop, watering_stopall_json):
+    """Test stopping all watering activities."""
+    async with authenticated_client:
+        authenticated_client.add(
+            '{0}:{1}'.format(TEST_HOST, TEST_PORT), '/api/4/watering/stopall',
+            'post',
+            aresponses.Response(
+                text=json.dumps(watering_stopall_json), status=200))
+
+        async with aiohttp.ClientSession(loop=event_loop) as websession:
+            client = await login(
+                TEST_HOST,
+                TEST_PASSWORD,
+                websession,
+                port=TEST_PORT,
+                ssl=False)
 
             data = await client.watering.stop_all()
             assert data['message'] == 'OK'
