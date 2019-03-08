7af80e256b6ceaacb63256d3feeab577774758f1
Aaron Bach <bachya1208@gmail.com>
2019-03-07 21:34:07 -0700

    Added test for getting inactive zones

--- tests/fixtures/zone.py
+++ tests/fixtures/zone.py
@@ -95,6 +95,44 @@ def zone_properties_json():
             "customSoilPreset": None,
             "customVegetationPreset": None,
             "customSprinklerPreset": None
+        },
+        {
+            "uid": 2,
+            "name": "Flower Bed",
+            "valveid": 1,
+            "ETcoef": 0.80000000000000004,
+            "active": False,
+            "type": 4,
+            "internet": True,
+            "savings": 100,
+            "slope": 1,
+            "sun": 1,
+            "soil": 5,
+            "group_id": 4,
+            "history": True,
+            "master": False,
+            "before": 0,
+            "after": 0,
+            "waterSense": {
+                "fieldCapacity": 0.17000000000000001,
+                "rootDepth": 229,
+                "minRuntime": -1,
+                "appEfficiency": 0.75,
+                "isTallPlant": True,
+                "permWilting": 0.029999999999999999,
+                "allowedSurfaceAcc": 8.3800000000000008,
+                "maxAllowedDepletion": 0.5,
+                "precipitationRate": 25.399999999999999,
+                "currentFieldCapacity": 16.030000000000001,
+                "area": 92.900001525878906,
+                "referenceTime": 1243,
+                "detailedMonthsKc": [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                "flowrate": None,
+                "soilIntakeRate": 10.16
+            },
+            "customSoilPreset": None,
+            "customVegetationPreset": None,
+            "customSprinklerPreset": None
         }]
     }
 
