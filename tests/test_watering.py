f036064126bcdf1e288dd155d4129924a554858c
Aaron Bach <bachya1208@gmail.com>
2020-01-28 17:48:51 -0700

    Migrated test_watering.py

--- tests/test_watering.py
+++ tests/test_watering.py
@@ -1,25 +1,16 @@
 """Define tests for restriction endpoints."""
-# pylint: disable=redefined-outer-name,too-many-arguments
-
 import datetime
-import json
 
 import aiohttp
 import pytest
 
 from regenmaschine import login
 
-from tests.const import TEST_HOST, TEST_PASSWORD, TEST_PORT
-from tests.fixtures import auth_login_json, authenticated_local_client
-from tests.fixtures.api import apiver_json
-from tests.fixtures.provision import provision_name_json, provision_wifi_json
-from tests.fixtures.watering import *
+from .common import TEST_HOST, TEST_PASSWORD, TEST_PORT, load_fixture
 
 
 @pytest.mark.asyncio
-async def test_watering_log_details(
-    aresponses, authenticated_local_client, event_loop, watering_log_json
-):
+async def test_watering_log_details(aresponses, authenticated_local_client):
     """Test getting watering log details."""
     today = datetime.date.today()
     today_str = today.strftime("%Y-%m-%d")
@@ -29,10 +20,12 @@ async def test_watering_log_details(
             f"{TEST_HOST}:{TEST_PORT}",
             f"/api/4/watering/log/details/{today_str}/2",
             "get",
-            aresponses.Response(text=json.dumps(watering_log_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_log_response.json"), status=200
+            ),
         )
 
-        async with aiohttp.ClientSession(loop=event_loop) as websession:
+        async with aiohttp.ClientSession() as websession:
             client = await login(
                 TEST_HOST, TEST_PASSWORD, websession, port=TEST_PORT, ssl=False
             )
@@ -42,25 +35,27 @@ async def test_watering_log_details(
 
 
 @pytest.mark.asyncio
-async def test_watering_pause(
-    aresponses, authenticated_local_client, event_loop, watering_pause_json
-):
+async def test_watering_pause(aresponses, authenticated_local_client):
     """Test pausing and unpausing watering."""
     async with authenticated_local_client:
         authenticated_local_client.add(
             f"{TEST_HOST}:{TEST_PORT}",
             "/api/4/watering/pauseall",
             "post",
-            aresponses.Response(text=json.dumps(watering_pause_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_pause_response.json"), status=200
+            ),
         )
         authenticated_local_client.add(
             f"{TEST_HOST}:{TEST_PORT}",
             "/api/4/watering/pauseall",
             "post",
-            aresponses.Response(text=json.dumps(watering_pause_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_pause_response.json"), status=200
+            ),
         )
 
-        async with aiohttp.ClientSession(loop=event_loop) as websession:
+        async with aiohttp.ClientSession() as websession:
             client = await login(
                 TEST_HOST, TEST_PASSWORD, websession, port=TEST_PORT, ssl=False
             )
@@ -73,19 +68,19 @@ async def test_watering_pause(
 
 
 @pytest.mark.asyncio
-async def test_watering_queue(
-    aresponses, authenticated_local_client, event_loop, watering_queue_json
-):
+async def test_watering_queue(aresponses, authenticated_local_client):
     """Test getting the watering queue."""
     async with authenticated_local_client:
         authenticated_local_client.add(
             f"{TEST_HOST}:{TEST_PORT}",
             "/api/4/watering/queue",
             "get",
-            aresponses.Response(text=json.dumps(watering_queue_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_queue_response.json"), status=200
+            ),
         )
 
-        async with aiohttp.ClientSession(loop=event_loop) as websession:
+        async with aiohttp.ClientSession() as websession:
             client = await login(
                 TEST_HOST, TEST_PASSWORD, websession, port=TEST_PORT, ssl=False
             )
@@ -95,9 +90,7 @@ async def test_watering_queue(
 
 
 @pytest.mark.asyncio
-async def test_watering_past(
-    aresponses, authenticated_local_client, event_loop, watering_past_json
-):
+async def test_watering_past(aresponses, authenticated_local_client):
     """Test gettinng info on past watering runs."""
     today = datetime.date.today()
     today_str = today.strftime("%Y-%m-%d")
@@ -107,10 +100,12 @@ async def test_watering_past(
             f"{TEST_HOST}:{TEST_PORT}",
             f"/api/4/watering/past/{today_str}/2",
             "get",
-            aresponses.Response(text=json.dumps(watering_past_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_past_response.json"), status=200
+            ),
         )
 
-        async with aiohttp.ClientSession(loop=event_loop) as websession:
+        async with aiohttp.ClientSession() as websession:
             client = await login(
                 TEST_HOST, TEST_PASSWORD, websession, port=TEST_PORT, ssl=False
             )
@@ -120,19 +115,19 @@ async def test_watering_past(
 
 
 @pytest.mark.asyncio
-async def test_watering_stop(
-    aresponses, authenticated_local_client, event_loop, watering_stopall_json
-):
+async def test_watering_stop(aresponses, authenticated_local_client):
     """Test stopping all watering activities."""
     async with authenticated_local_client:
         authenticated_local_client.add(
             f"{TEST_HOST}:{TEST_PORT}",
             "/api/4/watering/stopall",
             "post",
-            aresponses.Response(text=json.dumps(watering_stopall_json), status=200),
+            aresponses.Response(
+                text=load_fixture("watering_stopall_response.json"), status=200
+            ),
         )
 
-        async with aiohttp.ClientSession(loop=event_loop) as websession:
+        async with aiohttp.ClientSession() as websession:
             client = await login(
                 TEST_HOST, TEST_PASSWORD, websession, port=TEST_PORT, ssl=False
             )
