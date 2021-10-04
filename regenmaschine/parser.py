4d6a2615a5ea19fd71f765feda0c70ca15a97acb
shbatm <tim.j.bond@gmail.com>
2021-10-04 09:52:03 +0000

    Re-Type Post_Data

--- regenmaschine/parser.py
+++ regenmaschine/parser.py
@@ -1,6 +1,6 @@
 """Define an object to interact with RainMachine weather parsers."""
 import logging
-from typing import Any, Awaitable, Callable, Dict, cast
+from typing import Any, Awaitable, Callable, Dict, List, cast
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -17,7 +17,9 @@ class Parser:  # pylint: disable=too-few-public-methods
         data = await self._request("get", "parser")
         return cast(Dict[str, Any], data["parsers"])
 
-    async def post_data(self, json_payload: str) -> Dict[str, Any]:
+    async def post_data(
+        self, json_payload: Dict[str, List[Dict[str, Any]]]
+    ) -> Dict[str, Any]:
         """Post weather data from an external source.
 
         Local Weather Push service should be enabled from Settings > Weather >
