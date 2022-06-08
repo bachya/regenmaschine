8d6df59ee27b3908778779a0ba6189afaf3073a5
Aaron Bach <bachya1208@gmail.com>
2022-06-08 15:30:51 -0600

    Fix bug with programs/zones on 1st generation controllers

--- regenmaschine/zone.py
+++ regenmaschine/zone.py
@@ -64,9 +64,21 @@ class Zone:
         return {**results[0], **results[1]}
 
     async def start(self, zone_id: int, time: int) -> Dict[str, Any]:
-        """Start a program."""
-        return await self._request("post", f"zone/{zone_id}/start", json={"time": time})
+        """Start a zone.
+
+        Note that in addition to including it in the query URL, the zone ID must be
+        provided in the request body to accommodate 1st generation controllers.
+        """
+        return await self._request(
+            "post", f"zone/{zone_id}/start", json={"time": time, "zid": zone_id}
+        )
 
     async def stop(self, zone_id: int) -> Dict[str, Any]:
-        """Stop a program."""
-        return await self._request("post", f"zone/{zone_id}/stop")
+        """Stop a zone.
+
+        Note that in addition to including it in the query URL, the zone ID must be
+        provided in the request body to accommodate 1st generation controllers.
+        """
+        return await self._request(
+            "post", f"zone/{zone_id}/stop", json={"zid": zone_id}
+        )
