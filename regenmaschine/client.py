47015f5817ad3cc7cb0258615d5166949b1b42d7
J. Nick Koston <nick@koston.org>
2024-01-30 06:57:05 -1000

    Use URL objects for requests (#457)
    This avoid re-encoding the host every time
    
    Co-authored-by: Aaron Bach <bachya1208@gmail.com>

--- regenmaschine/client.py
+++ regenmaschine/client.py
@@ -9,6 +9,7 @@ from typing import Any, cast
 
 from aiohttp import ClientSession, ClientTimeout
 from aiohttp.client_exceptions import ClientOSError, ServerDisconnectedError
+from yarl import URL
 
 from .const import LOGGER
 from .controller import Controller, LocalController, RemoteController
@@ -58,7 +59,7 @@ class Client:
     async def _request(  # pylint: disable=too-many-arguments
         self,
         method: str,
-        url: str,
+        url: URL,
         *,
         access_token: str | None = None,
         access_token_expiration: datetime | None = None,
@@ -127,7 +128,7 @@ class Client:
         self,
         session: ClientSession,
         method: str,
-        url: str,
+        url: URL,
         use_ssl: bool,
         **kwargs: dict[str, Any],
     ) -> dict[str, Any]:
@@ -216,7 +217,7 @@ class Client:
         """
         auth_resp = await self._request(
             "post",
-            "https://my.rainmachine.com/login/auth",
+            URL("https://my.rainmachine.com/login/auth"),
             json={"user": {"email": email, "pwd": password, "remember": 1}},
         )
 
@@ -224,7 +225,7 @@ class Client:
 
         sprinklers_resp = await self._request(
             "post",
-            "https://my.rainmachine.com/devices/get-sprinklers",
+            URL("https://my.rainmachine.com/devices/get-sprinklers"),
             access_token=access_token,
             json={"user": {"email": email, "pwd": password, "remember": 1}},
         )
